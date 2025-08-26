ae502ed5b6dafd7ffab2d312a1d2960531a0c49f
sonjunho2 <sonjunho2@gmail.com>
2025-08-26 16:35:38 +0900

    Update users.controller.ts

--- services/api/src/modules/users/users.controller.ts
+++ services/api/src/modules/users/users.controller.ts
@@ -2,35 +2,35 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { PrismaService } from 'nestjs-prisma';
-import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/jwt.guard';
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
- constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
- @Get(':id')
- @UseGuards(JwtAuthGuard)
- @ApiBearerAuth()
- async byId(@Param('id') id: string) {
-   return this.prisma.user.findUnique({
-     where: { id },
-     select: {
-       id: true,
-       email: true,
-       displayName: true,
-       dob: true,
-       gender: true,
-       createdAt: true,
-       updatedAt: true,
-       provider: true,
-       region1: true,
-       region2: true,
-       lang: true,
-       trustScore: true,
-       status: true,
-       role: true,
-     },
-   });
- }
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async byId(@Param('id') id: string) {
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        displayName: true,
+        dob: true,
+        gender: true,
+        createdAt: true,
+        updatedAt: true,
+        provider: true,
+        region1: true,
+        region2: true,
+        lang: true,
+        trustScore: true,
+        status: true,
+        role: true,
+      },
+    });
+  }
 }
