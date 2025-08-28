028da19c9150670dccb09b86cb868436c988c6cd
sonjunho2 <sonjunho2@gmail.com>
2025-08-28 17:01:07 +0900

    Update app.module.ts

--- services/api/src/modules/app.module.ts
+++ services/api/src/modules/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
-
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
 import { ChatsModule } from './chats/chats.module';
 import { ReportsModule } from './reports/reports.module';
-
+import { MetricsModule } from './metrics/metrics.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt.guard';
 
@@ -20,6 +19,7 @@ import { JwtAuthGuard } from './auth/jwt.guard';
     UsersModule,
     ChatsModule,
     ReportsModule,
+    MetricsModule,
   ],
   providers: [
     {
