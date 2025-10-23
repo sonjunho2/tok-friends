e881b6a1584063352f9534e90b1eef82fa0595fe
sonjunho2 <sonjunho2@gmail.com>
2025-10-23 16:41:31 +0900

    Update app.module.ts

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'nestjs-prisma';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { HealthModule } from './modules/health/health.module';
@@ -9,10 +10,13 @@ import { ReportsModule } from './modules/reports/reports.module';
 import { MetricsModule } from './modules/metrics/metrics.module';
 import { AnnouncementsModule } from './modules/announcements/announcements.module';
 import { PostsModule } from './modules/posts/posts.module';
-import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/auth/jwt.guard';
 import { FriendshipsModule } from './modules/friendships/friendships.module';
 import { DiscoverModule } from './modules/discover/discover.module';
+import { TopicsModule } from './modules/topics/topics.module';
+import { CommunityModule } from './modules/community/community.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { GiftsModule } from './modules/gifts/gifts.module';
 
 @Module({
   imports: [
@@ -26,8 +30,12 @@ import { DiscoverModule } from './modules/discover/discover.module';
     MetricsModule,
     AnnouncementsModule,
     PostsModule,
+    TopicsModule,
+    CommunityModule,
     FriendshipsModule,
     DiscoverModule,
+    AdminModule,
+    GiftsModule,
   ],
   providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
